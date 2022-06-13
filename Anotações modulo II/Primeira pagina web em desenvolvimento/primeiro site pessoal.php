cd16456ba4714209d6b58d237345bf3d62576c3a
etherion <edson.etpl@gmail.com>
2022-06-13 16:00:50 -0300

    primeira versao pagina web

---
+++ Anotações modulo II/Primeira pagina web em desenvolvimento/primeiro site pessoal.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> Etherion </title>
+<link rel="stylesheet" href="style.css">
+</head>
+<aside> teste aside </aside>
+<body>
+<header>
+<h1 id="title">Etherion's blog</h1>
+</header>
+<img src="img1.jpg" alt="Layout exemplo da pagina">
+
+<section>
+<header>
+<h2 class="subtitle">sessões da pagina </h2>
+
+
+</header>
+
+<article  class="post">
+<h3 class="post_title"> primeira sessão</h3>
+
+<p> 
+What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been  the industry's standard dummy text ever since the 1500s,when an unknown printer
+<a href="https://web.dio.me/"
+ target=" _blank">took a galley of type and scrambled it to make a type specimen book.  </a> 
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+</p>
+</article>
+</section>
+
+
+
+<footer>
+<h2 class="post_title2"> Em caso de duvidas, contatar o Etherion nos seguintes locais </h2>
+<ul> 
+
+<li>
+<a href="mailto:edson.etpl@gmail.com">falar com Etherion atraves do email</a> <br>
+</li>
+<li>
+<a  href = "https://www.linkedin.com/in/edson-ribeiro-221a798a/" target=" _blank">falar com Etherion atraves do seu linkedin </a> <br>
+</li>
+<li> 
+<a href= "https://github.com/EtherionP" target=" _blank" >falar com o Etherion atraves do github</a> 
+<li>
+<a class="extra">em caso de dificuldade, segue imagem para facilitar reconhecimento <br>
+ <img src="img2.jpg" alt="imagem para reconhecer o contato" class="photo" >
+</li>
+</ul>
+</footer>
+</body>
+
+
+
+</html>
